f4442d206aa479fd116e3bfc08ef7ebb17de6481
Ryszard Czekaj <rychoo@freeshell.net>
2011-01-26 17:46:52 +0100

    Joomla Version 1.5.22 clean instalation

---
+++ configuration.php
@@ -0,0 +1,70 @@
+<?php
+class JConfig {
+/* Site Settings */
+var $offline = '0';
+var $offline_message = 'Witryna jest w trakcie prac konserwacyjnych.<br /> Zajrzyj później.';
+var $sitename = 'TeStEr';
+var $editor = 'tinymce';
+var $list_limit = '20';
+var $legacy = '0';
+/* Debug Settings */
+var $debug = '0';
+var $debug_lang = '0';
+/* Database Settings */
+var $dbtype = 'mysql';
+var $host = 'mysql3.000webhost.com';
+var $user = 'a1239518_rychoo';
+var $password = 'rabarbar2011';
+var $db = 'a1239518_joomla';
+var $dbprefix = 'jos_';
+/* Server Settings */
+var $live_site = '';
+var $secret = 'nQtGbBkOBsYKZPI2';
+var $gzip = '0';
+var $error_reporting = '-1';
+var $helpurl = 'http://help.joomla.org';
+var $xmlrpc_server = '0';
+var $ftp_host = 'czekaj.site90.net';
+var $ftp_port = '21';
+var $ftp_user = 'a1239518';
+var $ftp_pass = 'rabarbar2011';
+var $ftp_root = '/public_html';
+var $ftp_enable = '1';
+var $force_ssl = '0';
+/* Locale Settings */
+var $offset = '0';
+var $offset_user = '0';
+/* Mail Settings */
+var $mailer = 'mail';
+var $mailfrom = 'rycho@dd1studio.com';
+var $fromname = 'TeStEr';
+var $sendmail = '/usr/sbin/sendmail';
+var $smtpauth = '0';
+var $smtpsecure = 'none';
+var $smtpport = '25';
+var $smtpuser = '';
+var $smtppass = '';
+var $smtphost = 'localhost';
+/* Cache Settings */
+var $caching = '0';
+var $cachetime = '15';
+var $cache_handler = 'file';
+/* Meta Settings */
+var $MetaDesc = 'Joomla! - dynamiczny system portalowy i system zarządzania treścią';
+var $MetaKeys = 'joomla, pcj1x5y2z';
+var $MetaTitle = '1';
+var $MetaAuthor = '1';
+/* SEO Settings */
+var $sef           = '0';
+var $sef_rewrite   = '0';
+var $sef_suffix    = '0';
+/* Feed Settings */
+var $feed_limit   = 10;
+var $feed_email   = 'author';
+var $log_path = '/home/a1239518/public_html/logs';
+var $tmp_path = '/home/a1239518/public_html/tmp';
+/* Session Setting */
+var $lifetime = '15';
+var $session_handler = 'database';
+}
+?>
